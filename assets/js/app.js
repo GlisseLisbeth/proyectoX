9fc636261f28061bffb659d9c400a9b7a939ec0b
Maia <maia.rt.46@gmail.com>
2017-04-25 13:22:26 -0500

    Anadiendo Menu

--- assets/js/app.js
+++ assets/js/app.js
@@ -30,3 +30,26 @@ function color(){
   b=Math.round(Math.random()*255);
   return "rgb("+r+","+g+","+b+")";
 }
+/*
+  Slidemenu
+*/
+(function() {
+  var body = document.body
+  , menu = body.getElementsByClassName('menu')[0];
+
+  if ( typeof menu !== 'undefined' ) {
+    menu.addEventListener('click', function() {
+      body.className = ( body.className == 'menu-active' )? '' : 'menu-active';
+    });
+  }
+
+}).call(this);
+
+document.querySelector( "#nav-toggle" )
+  .addEventListener( "click", function(e) {
+    this.classList.toggle( "active" );
+    // e.target.parentNode.parentNode.parentNode.childNodes[3].style.width="80%";
+    // e.target.parentNode.parentNode.parentNode.childNodes[3].style.height="100%";
+    // e.target.parentNode.parentNode.parentNode.childNodes[3].style.zIndex="0";
+    // e.target.parentNode.parentNode.parentNode.childNodes[3].style.position="fixed";
+  });
